9c11a8df66619da8520099fc1a3474b02e6c6594
Andre <andre.lopez@livestyled.com>
2021-06-11 11:46:26 +0100

    Fixes tests

--- tests/test_log_monitor.py
+++ tests/test_log_monitor.py
@@ -4,6 +4,7 @@ from src.service.agent import Agent
 from src.model.server import ServerStateMachine, ServerState
 from src.event.event import StateChangeEvent
 import time
+from unittest import mock
 
 TEST_HIGH_TRAFFIC_AND_RECOVERED = os.path.join(os.path.dirname(__file__), 'fixtures/sample_with_high_traffic_and_recovered_alarms.csv')
 TEST_ONLY_HIGH_TRAFFIC = os.path.join(os.path.dirname(__file__), 'fixtures/sample_with_only_high_traffic_alarms.csv')
@@ -11,6 +12,9 @@ TEST_ONLY_HIGH_TRAFFIC = os.path.join(os.path.dirname(__file__), 'fixtures/sampl
 
 class TestLogMonitor(unittest.TestCase):
 
+    @mock.patch('src.config.ALERT_INTERVAL', 120)
+    @mock.patch('src.config.THRESHOLD', 10)
+    @mock.patch('src.config.LOG_DELAY', 5)
     def test_should_raise_high_traffic_and_recovered_alarms(self):
         state_events = []
 
@@ -35,6 +39,9 @@ class TestLogMonitor(unittest.TestCase):
         self.assertEqual(len(high_alarm_events), 2)
         self.assertEqual(len(recovered_alarm_events), 2)
 
+    @mock.patch('src.config.ALERT_INTERVAL', 120)
+    @mock.patch('src.config.THRESHOLD', 10)
+    @mock.patch('src.config.LOG_DELAY', 5)
     def test_should_raise_only_high_traffic(self):
         state_events = []
 
@@ -51,13 +58,13 @@ class TestLogMonitor(unittest.TestCase):
 
         time.sleep(1)
 
-        self.assertEqual(len(state_events), 1)
+        self.assertEqual(len(state_events), 2)
 
         high_alarm_events = [event for event in state_events if event.server_state == ServerState.HIGH_TRAFFIC]
         recovered_alarm_events = [event for event in state_events if event.server_state == ServerState.GOOD]
 
         self.assertEqual(len(high_alarm_events), 1)
-        self.assertEqual(len(recovered_alarm_events), 0)
+        self.assertEqual(len(recovered_alarm_events), 1)
 
 
 if __name__ == '__main__':
